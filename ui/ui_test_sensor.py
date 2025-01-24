db81b582789ccdd6e641db3a58a69b8a98752199
Daniyudha <dani.yudha33@gmail.com>
2025-01-24 17:52:09 +0700

    db-s

--- ui/ui_test_sensor.py
+++ ui/ui_test_sensor.py
@@ -53,27 +53,17 @@ class DbSensor(Db_connection):
             ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
             
-            # Data yang akan dimasukkan
-            # values = (
-            #     data.get("air_temp"), data.get("air_hum"), data.get("light_int"), data.get("rain_int"),
-            #     data.get("flow_1"), data.get("flow_2"), data.get("flow_3"), data.get("flow_4"),
-            #     data.get("water_ph"), data.get("water_ec"),
-            #     data.get("soil_1_hum"), data.get("soil_1_temp"), data.get("soil_1_ec"), data.get("soil_1_ph"),
-            #     data.get("soil_1_nitro"), data.get("soil_1_fosfor"), data.get("soil_1_kalium"),
-            #     data.get("soil_2_hum"), data.get("soil_2_temp"), data.get("soil_2_ec"), data.get("soil_2_ph"),
-            #     data.get("soil_2_nitro"), data.get("soil_2_fosfor"), data.get("soil_2_kalium"),
-            # )
-            
+            #Data yang akan dimasukkan
             values = (
-                data.get("temperature"), data.get("humidity"), data.get("lux"), data.get("rain_intensity"),
-                data.get("flow_rates[0]"), data.get("flow_rates[1]"), data.get("flow_rates[2]"), data.get("flow_rates[3]"),
-                data.get("water_ph"), data.get("ec_air"),
-                data.get("hum_tanah_1"), data.get("temp_tanah_1"), data.get("ec_tanah_1"), data.get("ph_tanah_1"),
-                data.get("nitro_1"), data.get("fosfor_1"), data.get("kalium_1"),
-                data.get("hum_tanah_2"), data.get("temp_tanah_2"), data.get("ec_tanah_2"), data.get("ph_tanah_2"),
-                data.get("nitro_2"), data.get("fosfor_2"), data.get("kalium_2"),
+                data.get("air_temp"), data.get("air_hum"), data.get("light_int"), data.get("rain_int"),
+                data.get("flow_1"), data.get("flow_2"), data.get("flow_3"), data.get("flow_4"),
+                data.get("water_ph"), data.get("water_ec"),
+                data.get("soil_1_hum"), data.get("soil_1_temp"), data.get("soil_1_ec"), data.get("soil_1_ph"),
+                data.get("soil_1_nitro"), data.get("soil_1_fosfor"), data.get("soil_1_kalium"),
+                data.get("soil_2_hum"), data.get("soil_2_temp"), data.get("soil_2_ec"), data.get("soil_2_ph"),
+                data.get("soil_2_nitro"), data.get("soil_2_fosfor"), data.get("soil_2_kalium"),
             )
-
+            
             # Eksekusi query
             self.cursor.execute(insert_query, values)
             self.mysql_connection.commit()
@@ -89,41 +79,69 @@ class DbSensor(Db_connection):
             self.cursor.close()
             self.mysql_connection.close()
 
-# if __name__ == "__main__":
-#     # Data dummy untuk pengujian
-#     dummy_data = {
-#         "air_temp": 25.5,
-#         "air_hum": 60.2,
-#         "light_int": 450.0,
-#         "rain_int": 0.0,
-#         "flow_1": 1.2,
-#         "flow_2": 1.1,
-#         "flow_3": 0.9,
-#         "flow_4": 1.3,
-#         "water_ph": 6.8,
-#         "water_ec": 1.5,
-#         "soil_1_hum": 25.0,
-#         "soil_1_temp": 24.5,
-#         "soil_1_ec": 0.8,
-#         "soil_1_ph": 6.5,
-#         "soil_1_nitro": 50.0,
-#         "soil_1_fosfor": 30.0,
-#         "soil_1_kalium": 40.0,
-#         "soil_2_hum": 22.5,
-#         "soil_2_temp": 23.8,
-#         "soil_2_ec": 0.7,
-#         "soil_2_ph": 6.4,
-#         "soil_2_nitro": 48.0,
-#         "soil_2_fosfor": 28.0,
-#         "soil_2_kalium": 35.0,
-#     }
+if __name__ == "__main__":
+    # Data dummy untuk pengujian
+    dummy_data = {
+        "air_temp": temperature,
+        "air_hum": humidity,
+        "light_int": lux,
+        "rain_int": rain_intensity,
+        "flow_1": flow_rates[0],
+        "flow_2": flow_rates[1],
+        "flow_3": flow_rates[2],
+        "flow_4": flow_rates[3],
+        "water_ph": water_ph,
+        "water_ec": ec_air,
+        "soil_1_hum": hum_tanah_1,
+        "soil_1_temp": temp_tanah_1,
+        "soil_1_ec": ec_tanah_1,
+        "soil_1_ph": ph_tanah_1,
+        "soil_1_nitro": nitro_1,
+        "soil_1_fosfor": fosfor_1,
+        "soil_1_kalium": kalium_1,
+        "soil_2_hum": hum_tanah_2,
+        "soil_2_temp": temp_tanah_2,
+        "soil_2_ec": ec_tanah_2,
+        "soil_2_ph": ph_tanah_2,
+        "soil_2_nitro": nitro_2,
+        "soil_2_fosfor": fosfor_2,
+        "soil_2_kalium": kalium_2,
+    }
 
-#     # Nama pengguna
 
-#     # Inisialisasi kelas DbSensor
-#     sensor_db = DbSensor()
+    # Nama pengguna
 
-#     # Menyimpan data dummy ke database
-#     sensor_db.save_sensor_data(dummy_data)
+    # Inisialisasi kelas DbSensor
+    sensor_db = DbSensor()
 
-#     print("Data dummy berhasil disimpan (jika tidak ada error).")
+    # Menyimpan data dummy ke database
+    sensor_db.save_sensor_data(dummy_data)
+
+    print("Data dummy berhasil disimpan (jika tidak ada error).")
+
+    # dummy_data = {
+    #     "air_temp": 25.5,
+    #     "air_hum": 60.2,
+    #     "light_int": 450.0,
+    #     "rain_int": 0.0,
+    #     "flow_1": 1.2,
+    #     "flow_2": 1.1,
+    #     "flow_3": 0.9,
+    #     "flow_4": 1.3,
+    #     "water_ph": 6.8,
+    #     "water_ec": 1.5,
+    #     "soil_1_hum": 25.0,
+    #     "soil_1_temp": 24.5,
+    #     "soil_1_ec": 0.8,
+    #     "soil_1_ph": 6.5,
+    #     "soil_1_nitro": 50.0,
+    #     "soil_1_fosfor": 30.0,
+    #     "soil_1_kalium": 40.0,
+    #     "soil_2_hum": 22.5,
+    #     "soil_2_temp": 23.8,
+    #     "soil_2_ec": 0.7,
+    #     "soil_2_ph": 6.4,
+    #     "soil_2_nitro": 48.0,
+    #     "soil_2_fosfor": 28.0,
+    #     "soil_2_kalium": 35.0,
+    # }
